f5c25aa333499a171afc62f6261a8f5774dd728c
GuusDeJonge <guusdj@hotmail.com>
2016-12-08 12:34:35 +0100

    cscccs

--- MultiClientServer/Program.cs
+++ MultiClientServer/Program.cs
@@ -8,7 +8,7 @@ namespace MultiClientServer
     class Program
     {
         static public int MijnPoort;
-        
+        //eded
         static public Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
 
         static void Main(string[] args)

8f46f9c2d02971389feec8e5785b21226b007c04
Tim Smit <t.smit2@students.uu.nl>
2016-12-12 13:29:08 +0100

    Program.cs aangepast om goed op te starten.

--- MultiClientServer/Program.cs
+++ MultiClientServer/Program.cs
@@ -2,51 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace MultiClientServer
 {
     class Program
     {
         static public int MijnPoort;
+        static int numberOfNeighbors;
 
         static public Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
 
         static void Main(string[] args)
         {
-            string testttttt = args[0];
-            Console.Write("Op welke poort ben ik server? ");
-            MijnPoort = int.Parse(Console.ReadLine());
+
+            Console.Title = args[0];
+            numberOfNeighbors = args.Length - 1;
+            
+            MijnPoort = int.Parse(args[0]);
             new Server(MijnPoort);
+
+            for(int i = 1; i < numberOfNeighbors; i++)
+            {
+                int port = int.Parse(args[i]);
+                Buren.Add(port, new Connection(port));
+            }
+
             
 
-            Console.WriteLine("Typ [verbind poortnummer] om verbinding te maken, bijvoorbeeld: verbind 1100");
-            Console.WriteLine("Typ [poortnummer bericht] om een bericht te sturen, bijvoorbeeld: 1100 hoi hoi");
-
-            while (true)
-            {
-                string input = Console.ReadLine();
-                if (input.StartsWith("verbind"))
-                {
-                    int poort = int.Parse(input.Split()[1]);
-                    if (Buren.ContainsKey(poort))
-                        Console.WriteLine("Hier is al verbinding naar!");
-                    else
-                    {
-                        // Leg verbinding aan (als client)
-                        Buren.Add(poort, new Connection(poort));
-                    }
-                }
-                else
-                {
-                    // Stuur berichtje
-                    string[] delen = input.Split(new char[] { ' ' }, 2);
-                    int poort = int.Parse(delen[0]);
-                    if (!Buren.ContainsKey(poort))
-                        Console.WriteLine("Hier is al verbinding naar!");
-                    else
-                        Buren[poort].Write.WriteLine(MijnPoort + ": " + delen[1]);
-                }
-            }
+            //while (true)
+            //{
+            //    string input = Console.ReadLine();
+            //    if (input.StartsWith("verbind"))
+            //    {
+            //        int poort = int.Parse(input.Split()[1]);
+            //        if (Buren.ContainsKey(poort))
+            //            Console.WriteLine("Hier is al verbinding naar!");
+            //        else
+            //        {
+            //            // Leg verbinding aan (als client)
+            //            Buren.Add(poort, new Connection(poort));
+            //        }
+            //    }
+            //    else
+            //    {
+            //        // Stuur berichtje
+            //        string[] delen = input.Split(new char[] { ' ' }, 2);
+            //        int poort = int.Parse(delen[0]);
+            //        if (!Buren.ContainsKey(poort))
+            //            Console.WriteLine("Hier is al verbinding naar!");
+            //        else
+            //            Buren[poort].Write.WriteLine(MijnPoort + ": " + delen[1]);
+            //    }
+            //}
         }
     }
 }

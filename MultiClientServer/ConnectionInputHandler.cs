5fdaf8ae8b81646d4cac3b0576a111951b008c47
Tim Smit <Tim Smit@DESKTOP-4LMJ4L5>
2016-12-23 16:38:02 +0100

    Inputhandler

---
+++ MultiClientServer/ConnectionInputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiClientServer
+{
+    public class ConnectionInputHandler : InputHandler
+    {
+        public override void B()
+        {
+            base.B();
+
+        }
+
+
+        public override void D()
+        {
+            base.D();
+        }
+
+        public override void myDist()
+        {
+            base.C();
+        }
+    }
+}
---
+++ MultiClientServer/InputHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiClientServer
+{
+    public class InputHandler
+    {
+        public virtual void C()
+        {
+
+        }
+
+        public virtual void D()
+        {
+
+        }
+
+        public virtual void B()
+        {
+
+        }
+
+        public virtual void myDist()
+        {
+
+        }
+    }
+}
